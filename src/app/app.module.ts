4662ca7c7285203189aec3cd5675ae3548685ced
vishwash-softrefine <vishwas.softrefine@gmail.com>
2021-06-23 19:52:10 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { OfferComponent } from './offer/offer.component';
+import { CardsComponent } from './cards/cards.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    OfferComponent,
+    CardsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  }
+  
+
+}
